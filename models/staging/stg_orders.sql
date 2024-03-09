35bbadc46e30cc80083019723b05ee3b91895cf1
Marcel Le <letan.dev@gmail.com>
2024-03-09 04:30:00 +0000

    add packages, dbt deps, and seed into stg_orders.sql

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,5 +1,7 @@
 select
     -- from raw orders
+    {{ dbt_utils.generate_surrogate_key(['o.orderid', 'o.customerid', 'o.productid']) }} as sk_orders, 
+    -- This macro implements a cross-database way to generate a hashed surrogate key using the fields specified.
     o.orderid,
     o.orderdate,
     o.shipdate,
@@ -16,9 +18,10 @@ select
     p.subcategory,
     o.ordersellingprice - o.ordercostprice as orderprofit,
     {{ markup()}} as markup,
-    {{ markup_with_arg('ordersellingprice', 'ordercostprice')}} as markup_with_argument
+    {{ markup_with_arg('ordersellingprice', 'ordercostprice')}} as markup_with_argument,
+    d.delivery_team
 from {{ ref("raw_orders") }} as o  -- raw.globalmart.orders
 left join {{ ref("raw_customer") }} as c on o.customerid = c.customerid
 left join {{ ref("raw_product") }} as p on o.productid = p.productid
-
+left join {{ ref('delivery_team') }} as d on d.shipmode = o.shipmode
 -- {{ limit_data_in_dev('orderdate') }} -- to run a macro function to display only the orderid rows with order date under 30 days
