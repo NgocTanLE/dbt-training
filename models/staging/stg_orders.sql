338c6d919fe91f1a6af49ff2490986718cc57b33
Marcel Le <letan.dev@gmail.com>
2024-03-09 04:10:54 +0000

    add macros

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -14,7 +14,11 @@ select
     p.category,
     p.productname,
     p.subcategory,
-    o.ordersellingprice - o.ordercostprice as orderprofit
+    o.ordersellingprice - o.ordercostprice as orderprofit,
+    {{ markup()}} as markup,
+    {{ markup_with_arg('ordersellingprice', 'ordercostprice')}} as markup_with_argument
 from {{ ref("raw_orders") }} as o  -- raw.globalmart.orders
 left join {{ ref("raw_customer") }} as c on o.customerid = c.customerid
 left join {{ ref("raw_product") }} as p on o.productid = p.productid
+
+-- {{ limit_data_in_dev('orderdate') }} -- to run a macro function to display only the orderid rows with order date under 30 days
